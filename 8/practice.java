e1401478fef3ee1b4fe8f661fd7c7bc911b41f6e
Prashant Thorat <prashant.workemail0@gmail.com>
2026-07-14 00:49:03 +0530

    Patterns Practice

---
+++ 8/practice.java
@@ -0,0 +1,171 @@
+import java.util.*;
+public class practice {
+    public static void main(String[] arg){
+
+        // line Star
+        // try(Scanner sc = new Scanner(System.in)){
+        //     System.out.print("Enter Number:");
+        //     int n = sc.nextInt();
+
+        //     for(int i=1;i<=n;i++){
+        //         System.out.println("*");
+        //     }
+        // }
+
+        // triangle star
+        // try(Scanner sc = new Scanner(System.in)){
+        //     System.out.print("Enter Number:");
+        //     int n = sc.nextInt();
+
+        //     for(int i=1;i<=n;i++){
+        //         for(int j = 1; j<=i;j++){
+        //             System.out.print("* ");
+        //         }
+        //         System.out.print("\n");
+        //     }
+
+        // }
+
+
+        //Inverted triangle
+        // try(Scanner sc = new Scanner(System.in)){
+        //     System.out.print("Enter Number: ");
+
+        //     int n = sc.nextInt();
+        //     for(int i =1; i<=n;i++){
+        //         for(int j=1; j<=n-i+1;j++){
+        //             System.out.print("* ");
+        //         }
+        //         System.out.print("\n");
+        //     }
+        // }
+
+        //Number triangle
+        // try(Scanner sc = new Scanner(System.in)){
+        //     System.out.print("Enter Number: ");
+
+        //     int n = sc.nextInt();
+        //     for(int i =1; i<=n;i++){
+        //         for(int j=1; j<=i;j++){
+        //             System.out.print(j+ " ");
+        //         }
+        //         System.out.print("\n");
+        //     }
+        // }
+
+
+        //    *
+        //   **
+        //  ***
+        // ****
+        // try(Scanner sc = new Scanner(System.in)){
+        //     System.out.print("Enter Number: ");
+
+        //     int n = sc.nextInt();
+        //     for(int i =1; i<=n;i++){
+        //         for(int j=1; j<=n-i;j++){
+        //             System.out.print(" ");
+        //         }
+        //         // System.out.print("\n");
+        //         for(int k=1;k<=i;k++){
+        //             System.out.print("*");
+        //         }
+        //         System.out.print("\n");
+        //     }
+        // }
+
+
+
+        // *****
+        //  ****
+        //   ***
+        //    **
+        //     *
+        // try(Scanner sc = new Scanner(System.in)){
+        //     System.out.print("Enter Number: ");
+
+        //     int n = sc.nextInt();
+        //     for(int i =1; i<=n;i++){
+        //         for(int j=1; j<=i-1;j++){
+        //             System.out.print(" ");
+        //         }
+        //         // System.out.print("\n");
+        //         for(int k=1;k<=n-i+1;k++){
+        //             System.out.print("*");
+        //         }
+        //         System.out.print("\n");
+        //     }
+        // }
+
+
+
+        // triangle 
+        // try(Scanner sc = new Scanner(System.in)){
+        //     System.out.print("Enter Number: ");
+
+        //     int n = sc.nextInt();
+        //     for(int i =1; i<=n;i++){
+        //         for(int j=1; j<=n-i;j++){
+        //             System.out.print(" ");
+        //         }
+        //         // System.out.print("\n");
+        //         for(int k=1;k<=2*i-1;k++){
+        //             System.out.print("*");
+        //         }
+        //         // for(int l=1;l<=i-1;l++){
+        //         //     System.out.print("*");
+        //         // }
+        //         System.out.print("\n");
+        //     }
+        // }
+
+
+        // try(Scanner sc = new Scanner(System.in)){
+        //     System.out.print("Enter Number: ");
+
+        //     int n = sc.nextInt();
+        //     for(int i=1;i<=n;i++){
+        //         for(int j=1;j<=i;j++){
+        //             System.out.print("*");
+        //         }
+        //         System.out.print("\n");
+        //     }
+        //     for(int i=1;i<=n-1;i++){
+        //         for(int j=1;j<=n-i;j++){
+        //             System.out.print("*");
+        //         }
+        //         System.out.print("\n");
+        //     }
+        // }
+
+
+
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.print("Enter Number: ");
+
+            int n = sc.nextInt();
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n-i;j++){
+                    System.out.print(" ");
+                }
+                for(int k=1;k<=i;k++){
+                    System.out.print("*");
+                }
+                System.out.print("\n");
+            }
+            for(int i=1;i<=n-1;i++){
+                for(int j=1;j<=i;j++){
+                    System.out.print(" ");
+                }
+                for(int k=1;k<=n-i;k++){
+                    System.out.print("*");
+                }
+                System.out.print("\n");
+            }
+        }
+
+
+        
+
+    }
+}
---
+++ 8/tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+for(int k=1;k<=2*n-i;k++){
+                //     System.out.print("*");
+                // }
